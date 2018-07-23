c5f109bcd5be663052054fd0eb74788fdea4d511
Stefan Berger <stefanb@linux.vnet.ibm.com>
2018-07-23 12:01:08 -0400

    Change from Architecture to Platform

--- images/image.go
+++ images/image.go
@@ -70,8 +70,8 @@ type LayerInfo struct {
 	Encryption string
 	// The size of the layer file
 	FileSize int64
-	// The architecture for which this layer is
-	Architecture string
+	// The platform for which this layer is
+	Platform string
 }
 
 // DeleteOptions provide options on image delete
@@ -552,7 +552,7 @@ func CryptManifestList(ctx context.Context, cs content.Store, desc ocispec.Descr
 func GetImageLayerInfo(ctx context.Context, cs content.Store, desc ocispec.Descriptor) ([]LayerInfo, error) {
 	var (
 		lis []LayerInfo
-		Architecture string
+		Platform string
 	)
 
 	switch (desc.MediaType) {
@@ -560,7 +560,7 @@ func GetImageLayerInfo(ctx context.Context, cs content.Store, desc ocispec.Descr
 		MediaTypeDockerSchema2Manifest, ocispec.MediaTypeImageManifest:
 		children, err := Children(ctx, cs, desc)
 		if desc.Platform != nil {
-			Architecture = desc.Platform.Architecture
+			Platform = desc.Platform.OS + "/" + desc.Platform.Architecture
 		}
 		if err != nil {
 			return []LayerInfo{}, err
@@ -572,8 +572,8 @@ func GetImageLayerInfo(ctx context.Context, cs content.Store, desc ocispec.Descr
 			}
 			for i := 0; i < len(tmp); i++ {
 				tmp[i].Id = uint32(i)
-				if Architecture != "" {
-					tmp[i].Architecture = Architecture
+				if Platform != "" {
+					tmp[i].Platform = Platform
 				}
 			}
 			lis = append(lis, tmp...)
