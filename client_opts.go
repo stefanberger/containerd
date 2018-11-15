9642d44cf76e33c33f853df6cb16b033ae5b3008
Harshal Patil <harshal.patil@in.ibm.com>
2018-11-15 21:46:24 +0530

    Support image authorization check during pulling the image
    Signed-off-by: Harshal Patil <harshal.patil@in.ibm.com>

--- client_opts.go
+++ client_opts.go
@@ -180,7 +180,7 @@ func WithImageHandler(h images.Handler) RemoteOpt {
 }
 
 // WithDecryptionKeys sets private keys in client struct
-func WithDecryptionKeys(decryptionKeys string) RemoteOpt {
+func WithDecryptionKeys(decryptionKeys []string) RemoteOpt {
 	return func(client *Client, c *RemoteContext) error {
 		client.decryptionKeys = decryptionKeys
 		return nil
